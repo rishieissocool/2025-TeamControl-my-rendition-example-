c29e1904cded3f507d1aa3de5f8c677eb22095c7
Emma Tsang <20544166@student.westernsydney.edu.au>
2024-10-17 17:42:22 +1100

    added more import packages

--- src/TeamControl/Model/__init__.py
+++ src/TeamControl/Model/__init__.py
@@ -1,6 +1,8 @@
-__all___ = [World,Field,Arc,Lines, Frame, Robot, Ball, world2robot, robot2world]
+__all___ = [World,Field,Arc,Lines, Frame, Robot, Ball, Nearest, world2robot, robot2world, predict_trajectory]
 
 from .field import Field,Arc,Lines
 from .frame import Frame, Robot, Ball
 from .world import World
 from .transform_cords import world2robot, robot2world
+from .nearest import Nearest
+from .Trajectory import predict_trajectory
