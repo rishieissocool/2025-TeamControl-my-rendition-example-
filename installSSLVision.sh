f5ae7dc49e8c86461fb385e624b19620fbca4cd1
Emma-ZED <20544166@student.westernsydney.edu.au>
2025-03-19 16:32:50 +1100

    removed wireshark from script. Do not install using terminal.

--- installSSLVision.sh
+++ installSSLVision.sh
@@ -22,14 +22,19 @@ echo "Installing SSL Vision please do not touch until you see the phrase - 'END'
         SSL_DIR="$HOME/ssl-software"
         VISION_DIR="$SSL_DIR/ssl-vision"
 
+        echo "searching for Directory :$SSL_DIR"
+        
         if [ ! -d "$SSL_DIR" ]; then
             echo "Directory : $SSL_DIR not found. Creating directory . . ."
             mkdir -p "$SSL_DIR" || { echo "Fail to create Directory $SSL_DIR"; exit 1; }
+        else 
+            echo "Found $SSL_DIR"
         fi
 
         cd "$SSL_DIR" || { echo "Failed to cd into $SSL_DIR"; exit 1;}
         
         echo "*** Installing ssl Vision --> $VISION_DIR"
+
         if [ ! -d "$VISION_DIR" ]; then
             echo "cloning SSL vision from GitHub"
             git clone https://github.com/RoboCup-SSL/ssl-vision.git || { echo "Git clone failed"; exit 1; }
@@ -43,9 +48,9 @@ echo "Installing SSL Vision please do not touch until you see the phrase - 'END'
         ./InstallPackagesUbuntu.sh || { echo "Vision Dependencies installation failed"; exit 1; }
 
         ## installing v4l for usb camera
-        sudo apt install -y v4l-utils wireshark
+        sudo apt install -y v4l-utils
         
-        echo " Building Vision base on using USB camera Input "
+        echo "Building Vision base on using USB camera Input"
         ## Setting vision to work with USB camera
         cmake -B build -DUSE_V4L=true
         ## install
