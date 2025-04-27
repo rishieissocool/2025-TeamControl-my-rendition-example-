c9d29319741ed463e94787091ed4081d3efb96f9
Emmama222 <20544166@student.westernsydney.edu.au>
2025-04-27 17:51:57 +1000

    fixed import ..

--- src/TeamControl/SSL/GameControl/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["GameControl_recv","RefereeMessage"]
-
-from TeamControl.SSL.GameControl.Receiver import GameControl as GameControl_recv
-from TeamControl.SSL.GameControl.Processing import *
-from TeamControl.SSL.GameControl.Message import RefereeMessage
