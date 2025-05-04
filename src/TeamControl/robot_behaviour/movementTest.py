dd35272e8feea89726cecb1ca674c653acc73c48
AliveDPG <darpangurung187@gmail.com>
2025-05-05 02:20:01 +1000

    Added mroe cmments

--- src/TeamControl/robot_behaviour/movementTest.py
+++ src/TeamControl/robot_behaviour/movementTest.py
@@ -7,8 +7,11 @@ def get_path_point_test():
     # path = ([1,1])
     robot_pos = [1, 1]
     Fp.update_path(path) # update the path into a class 
-    a = Fp.get_point(robot_pos) #then you can call the function to get a traget
-    print(a)
+    target = Fp.get_point(robot_pos) #then you can call the function to get a traget
+    #^ idea just set a path to eatch robot which we can update when we want 
+    
+    
+    print(target)
 
 
 
