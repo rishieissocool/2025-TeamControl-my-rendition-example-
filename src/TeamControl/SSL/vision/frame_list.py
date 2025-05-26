9c55ba72bbb2bd0f979c6592c02bd072a5d25009
Emma <20544166@student.westernsydney.edu.au>
2025-05-26 16:18:31 +1000

    broken down frame vs frame_list

---
+++ src/TeamControl/SSL/vision/frame_list.py
@@ -0,0 +1,87 @@
+from TeamControl.SSL.vision.frame import Frame
+from collections import deque
+
+import numpy as np
+import numpy.typing as npt
+
+class FrameList ():
+    
+    GRSIM_CAMERAS = 4
+    REAL_CAMERAS = 1
+    
+    ### THIS IS A LIST CLASS so this would work like a list
+    def __init__(self,use_sim:bool=True,history:int=60):
+        self.newest_frame = 0
+        self.use_sim = use_sim
+        self.history = history
+        self._frames = deque(maxlen=history)
+        self._frame_lookup = {}  # Maps frame_id -> Frame
+    
+    
+    def __repr__(self):
+        return repr(self._frames)
+    
+    @property 
+    def max_num(self):
+        return self.history * self.cameras
+    
+    @property
+    def cameras(self):
+        return self.GRSIM_CAMERAS if self.use_sim else self.REAL_CAMERAS
+    
+    @property
+    def is_complete(self):
+        return self.latest.is_completed if isinstance(self.latest,Frame) else False
+    
+
+    @property
+    def frame_ids(self) -> list[int]:
+        return [frame.frame_number for frame in self._frames]    
+    
+    @property
+    def latest(self) -> Frame | None:
+        return self._frames[-1] if self._frames else None
+
+    def update(self, new_detection):
+        if new_detection.frame_number == self.newest_frame:
+            self.latest.update(new_detection)
+        elif new_detection.frame_number > self.newest_frame:
+            self.newest_frame = new_detection.frame_number
+            frame = Frame.from_proto(new_detection,max_cameras=self.cameras)
+            self.append(frame)
+    
+        
+    def append(self, frame: Frame):
+        if frame.frame_number in self._frame_lookup:
+            raise LookupError (f"{frame.frame_number} exist, use update instead")
+            self.update(frame)
+        # If full, remove oldest from both deque and dict
+        if len(self._frames) == self.history:
+            old_frame = self._frames.popleft() 
+            del self._frame_lookup[old_frame.frame_number] 
+        # Add new frame
+        self._frames.append(frame)
+        self._frame_lookup[frame.frame_number] = frame # adds to dictionary
+
+    def get_frame_withid(self, frame_id: int) -> Frame | None:
+        return self._frame_lookup.get(frame_id)
+
+    def get_last_n_frames(self, n: int) -> list[Frame]:
+        return list(self._frames)[-n:]
+
+    
+    def clear(self):
+        self._frames.clear()
+    
+    # * index is 0-100, not frame number 
+    def __getitem__(self, index):
+        return self._frames[index]
+
+    def __setitem__(self, index, value):
+        self._frames[index] = value
+
+    def __len__(self):
+        return len(self._frames)
+
+    def __iter__(self):
+        return iter(self._frames)
