d2cc8a4c99799bb500e2bcb12f842ea2e0efcd5f
Emma Tsang <20544166@student.westernsydney.edu.au>
2025-07-07 01:38:49 +1000

    changed vision process back to vision worker

--- src/TeamControl/main.py
+++ src/TeamControl/main.py
@@ -1,5 +1,5 @@
 from multiprocessing import Process, Queue,Event
-from TeamControl.SSL.vision.Process import VisionProcess
+from TeamControl.SSL.vision.Process import vision_worker
 from TeamControl.SSL.game_controller.compare import game_controller_worker
 from TeamControl.world.model_manager import WorldModelManager
 from TeamControl.world.model_runner import wm_runner
@@ -18,7 +18,7 @@ def main():
     wm = wm_manager.WorldModel()
     
     
-    vision_wkr = Process(target=VisionProcess, args=(vision_q,use_sim,))
+    vision_wkr = Process(target=vision_worker, args=(vision_q,use_sim,))
     gc_wkr = Process(target=game_controller_worker, args=(gc_q,))
     
     wmr = Process(target=wm_runner, args=(wm,vision_q,gc_q,))
