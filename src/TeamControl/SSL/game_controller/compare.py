43bb38fac1d60940342c4ffe71c5c78d430287a4
Emma <20544166@student.westernsydney.edu.au>
2025-07-07 00:11:44 +1000

    added Game Controller Multirprocessing. Tested. INDIVIDUALLY

---
+++ src/TeamControl/SSL/game_controller/compare.py
@@ -0,0 +1,79 @@
+from TeamControl.SSL.game_controller.Message import *
+from TeamControl.network.gameControllerSockets import GameControl
+from multiprocessing import Queue
+
+class GameControllerManager:
+    def __init__(self,output_q:Queue,):
+        self.recv = GameControl()
+        self.output_q = output_q
+        self.ref_msg:RefereeMessage = None
+        
+    def run(self):
+        while True:
+            new_ref = self.listen()
+            self.update(new_ref)
+    
+    def listen(self):
+        data, _ = self.recv.listen()
+        ref_msg = self.convert(data)
+        return ref_msg
+     
+    def update(self,ref_msg:RefereeMessage):
+        current = self.ref_msg
+        if current is None:
+            print("initialise")
+            self.ref_msg = ref_msg
+            self.output_q.put(ref_msg)
+            return
+        elif current is not None:
+            if current.command != ref_msg.command:
+                self.output_q.put(ref_msg.command)
+            if current.stage != ref_msg.stage:
+                self.output_q.put(ref_msg.stage)
+            if current.blue_team_on_positive_half != ref_msg.blue_team_on_positive_half:
+                print("sides switched")
+                self.output_q.put(ref_msg)
+            if current.yellow.name == ref_msg.blue.name:
+                print(f"Team {current.yellow.name} has switch to blue")
+                self.output_q.put(ref_msg.blue)
+            if current.blue.name == ref_msg.yellow.name:
+                print(f"Team {current.blue.name} has switch to yellow")
+                self.output_q.put(ref_msg.yellow)
+                
+            if current.yellow.max_allowed_bots < ref_msg.yellow.max_allowed_bots:
+                self.output_q.put(ref_msg.yellow) 
+
+            if current.blue.max_allowed_bots < ref_msg.blue.max_allowed_bots:
+                self.output_q.put(ref_msg.yellow)
+            self.ref_msg = ref_msg
+        else:
+            return
+        
+    def convert(self,ref_msg):
+        try:
+            ref_msg = RefereeMessage.from_proto(ref_msg)
+            return ref_msg
+        except Exception as e:
+            raise Exception(f"exception encountered : {ExceptionGroup}{e}")
+
+
+def game_controller_worker(output_q):
+    manager = GameControllerManager(output_q)
+    manager.run()
+
+if __name__ == "__main__":
+    def read(input_q):
+        while True:
+            if not input_q.empty():
+                item = input_q.get_nowait()
+                print(item)
+    from multiprocessing import Process
+    output_q = Queue()
+    
+    manager = Process(target=game_controller_worker,args=(output_q,))
+    reader = Process(target=read,args=(output_q,))
+    
+    manager.start()
+    reader.start()
+    manager.join()
+    reader.join()
