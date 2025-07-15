14193ad4284fb928f35281465cbd86c1ad0de566
Emma <20544166@student.westernsydney.edu.au>
2025-07-15 23:45:24 +1000

    added stuff for goalie testing in grSim

--- src/TeamControl/main.py
+++ src/TeamControl/main.py
@@ -5,11 +5,12 @@ from TeamControl.world.model_manager import WorldModelManager
 from TeamControl.world.model_runner import wm_runner
 from TeamControl.utils.dummy_process import DummyReader
 from TeamControl.utils.remote_control_process import RCProcess,run_rc_process
+from TeamControl.robot.goalie import run_goalie
 
 # in multiprocessing this can only be a simple process
 
 def main():
-    use_sim = False
+    use_sim = True
     vision_q = Queue()
     gc_q = Queue()
     
@@ -22,18 +23,18 @@ def main():
     gc_wkr = Process(target=game_controller_worker, args=(gc_q,))
     
     wmr = Process(target=wm_runner, args=(wm,vision_q,gc_q,))
-    some_other_process = Process(target=run_rc_process,args=(wm,))
+    some_other_process = Process(target=run_goalie,args=(wm,5))
     # some_other_process2 = Process(target=DummyReader,args=(wm,))
     vision_wkr.start()
     gc_wkr.start()
     wmr.start()
-    # some_other_process.start()
+    some_other_process.start()
     # some_other_process2.start()
     
     vision_wkr.join()
     gc_wkr.join()
     wmr.join()
-    # some_other_process.join()
+    some_other_process.join()
     # some_other_process2.join()
 
 if __name__ == "__main__":
