04a648752f2e5c4f21f3437aec11f3fb86e3d30b
Emma Tsang <20544166@student.westernsydney.edu.au>
2025-08-02 02:32:36 +1000

    changed location of things, and renamed some_process to goalie

--- src/TeamControl/main.py
+++ src/TeamControl/main.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process, Queue,Event
 from TeamControl.SSL.vision.Process import vision_worker
-from TeamControl.SSL.game_controller.compare import game_controller_worker
+# from TeamControl.SSL.game_controller.compare import game_controller_worker
+from TeamControl.SSL.game_controller.fsm import run_gcfsm
 from TeamControl.world.model_manager import WorldModelManager
 from TeamControl.world.model_runner import wm_runner
 from TeamControl.utils.dummy_process import DummyReader
@@ -13,28 +14,30 @@ def main():
     use_sim = True
     vision_q = Queue()
     gc_q = Queue()
+    # robot_feedback_q = Queue()
     
+    # inputs
+    vision_wkr = Process(target=vision_worker, args=(vision_q,use_sim,))
+    gc_wkr = Process(target=run_gcfsm, args=(gc_q,))
+    
+    # world model
     wm_manager = WorldModelManager()
     wm_manager.start()
     wm = wm_manager.WorldModel()
-    
-    
-    vision_wkr = Process(target=vision_worker, args=(vision_q,use_sim,))
-    gc_wkr = Process(target=game_controller_worker, args=(gc_q,))
-    
     wmr = Process(target=wm_runner, args=(wm,vision_q,gc_q,))
-    some_other_process = Process(target=run_goalie,args=(wm,5))
+    
+    goalie = Process(target=run_goalie,args=(wm,5))
     # some_other_process2 = Process(target=DummyReader,args=(wm,))
     vision_wkr.start()
     gc_wkr.start()
     wmr.start()
-    some_other_process.start()
+    goalie.start()
     # some_other_process2.start()
     
     vision_wkr.join()
     gc_wkr.join()
     wmr.join()
-    some_other_process.join()
+    goalie.join()
     # some_other_process2.join()
 
 if __name__ == "__main__":
