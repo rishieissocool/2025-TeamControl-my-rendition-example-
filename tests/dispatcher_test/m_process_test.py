0da0f4ab28566334e440506b904186c020ffd3cc
Emma <20544166@student.westernsydney.edu.au>
2025-10-12 21:15:39 +1100

    updated goalie, remote process to work with dispatcher (testing needed)

--- tests/dispatcher_test/m_process_test.py
+++ tests/dispatcher_test/m_process_test.py
@@ -7,8 +7,9 @@ from TeamControl.dispatcher.generate_packet import generate_w_interval
 
 if __name__ == "__main__":
     q = Queue()
+    is_yellow = True
     input = Process(target=generate_w_interval, args=(q,))
-    output = Process(target=run_dispatcher, args=(q,))
+    output = Process(target=run_dispatcher, args=(q,False,is_yellow,))
     
     input.start()
     output.start()
