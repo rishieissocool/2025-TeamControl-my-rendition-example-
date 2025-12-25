bff6bd08fa997250635cd04b873d2969c1d23720
Emma <20544166@student.westernsydney.edu.au>
2025-12-26 00:40:02 +1100

    modified to be a sub class of Base Worker, works and tested

--- src/TeamControl/process_workers/vision_runner.py
+++ src/TeamControl/process_workers/vision_runner.py
@@ -3,29 +3,23 @@ from TeamControl.SSL.vision.field import GeometryData
 from TeamControl.network.ssl_sockets import Vision,VisionTracker
 from TeamControl.utils.Logger import LogSaver
 
-from TeamControl.network.ssl_sockets import Vision,VisionTracker
-from TeamControl.utils.Logger import LogSaver
+from TeamControl.process_workers.worker import BaseWorker
 
 import numpy as np
-import numpy.typing as npt
 import time
-from multiprocessing import Queue, Process
 
 
 
-class VisionProcess():
+class VisionProcess(BaseWorker):
     GRSIM_CAMERAS = 4
     REAL_LIFE_CAMERAS = 1
     
-    def __init__(self,is_running,output_q:Queue,logger,use_grSim:bool=True,vision_port=10006):
-        self.logger:LogSaver = logger
-        self.use_grSim = use_grSim
-        self.output_q = output_q
-        self.recv = Vision(is_running=is_running,port=vision_port)    
+    def __init__(self,is_running,logger):
+        super().__init__(is_running=is_running,logger=logger)
+        self.loop_timer = None
         self.field = None
         self.frame = None
         self.frame_number = -1
-        self.logger.info(f"now listening on {vision_port}, using grSim ? {use_grSim} cameras : {self.cameras}")
         self.error_loop_count =0 
         
     @property
@@ -36,68 +30,98 @@ class VisionProcess():
     def has_field(self):
         return self.field is not None
     
+    def setup(self,*args):
+        output_q, use_grSim, vision_port = args
+        self.use_grSim = use_grSim
+        self.output_q = output_q
+        self.recv = Vision(is_running=self.is_running,port=vision_port)    
+        self.logger.info(f"[VP] : now listening on {vision_port}, using grSim ? {use_grSim} cameras : {self.cameras}")
 
-    def run(self,is_running) -> bool:
-        while is_running.is_set():
-            try:
-                new_vision_data = self.recv.listen()
-                if new_vision_data is None: 
-                    # if we want to shut it down after trying ? 
-                    self.error_loop_count += 1
-                    if self.error_loop_count > 5:
-                        self.logger.error("This is None ? ")
-                        self.error_loop_count = 0
-                        # exit()
-                    continue
+
+    def step(self) -> bool:
+        # listen for data
+        new_vision_data = self.recv.listen()
+        # if after timeout it is none
+        if new_vision_data is None:
+            self.logger.warning("[VP] : No vision data received")
+            # we don't have data, so return false 
+            return
                 
-                if new_vision_data.HasField("detection"):
-                    new_detection_data = new_vision_data.detection
-                    if self.frame_number < new_detection_data.frame_number:
-                        self.logger.info(f"We get new frame : {new_detection_data.frame_number}")
-                        # generates new frame
-                        self.frame = Frame.from_proto(new_detection_data,self.cameras)
-                        self.frame_number = self.frame.frame_number
-                    
-                    # if same frame number = it is old frame
-                    if self.frame_number == new_detection_data.frame_number:
-                        # we update the original frame
-                        self.frame.update(new_detection_data)
-                        if self.frame.is_completed:
-                            self.logger.info(f"frame: {self.frame_number} has been completed with {self.cameras} cameras , time taken = {time.time() - loop_timer}")
-                            self.send(self.frame)
-                if new_vision_data.HasField("geometry"):
-                    geometry = new_vision_data.geometry
-                    self.field = GeometryData.from_proto(geometry)
-                    self.logger.info(f"frame: {self.frame_number} has geometry")
-                    self.send(self.field)
-                loop_timer = time.time()
-            except KeyboardInterrupt:
-                continue
+        # if we received the vision data and it has type Detection: 
+        if new_vision_data.HasField("detection"):
+            self.update_detection(new_vision_data.detection)
+                
+        if new_vision_data.HasField("geometry"):
+            self.update_geometry(new_vision_data.geometry)
+            
+        self.loop_timer = time.time()
+    
+    def update_geometry(self,new_geometry):
+        # replace variable for simplicity
+        self.field = GeometryData.from_proto(new_geometry)
+        self.logger.info(f"[VP] : frame: {self.frame_number} has geometry")
+        self.send(self.field)
+    
+    def update_detection(self,new_detection_data):
+        # if this frame number is older than the new frame
+        if self.frame_number < new_detection_data.frame_number:
+            self.logger.info(f"[VP] :We get new frame : {new_detection_data.frame_number}")
+            # generates new frame
+            self.frame = Frame.from_proto(new_detection_data,self.cameras)
+            self.frame_number = self.frame.frame_number
         
-        print("vision_process quit")
+        # if same frame number = it is old frame
+        elif self.frame_number == new_detection_data.frame_number:
+            # we update the original frame
+            self.logger.info(f"[VP] : Updating old frame : {new_detection_data.frame_number}")
+            self.frame.update(new_detection_data)
+        
+        # if the frame is now completed 
+        if self.frame.is_completed is True:
+            self.logger.info(f"[VP] : frame: {self.frame_number} has been completed with {self.cameras} cameras , time taken = {time.time() - self.loop_timer}")
+            self.send(self.frame)
     
     def send(self,data):
         if not self.output_q.full():
             self.output_q.put(data)
         else:
-            self.logger.warning("QUEUE IS FULL")
+            self.logger.warning("[VP] : VISION QUEUE IS FULL")
 
-def vision_worker(is_running,output_q:Queue,use_grSim:bool=True,vision_port=10006):
-    logger = LogSaver()
-    v = VisionProcess(is_running,output_q,logger,use_grSim,vision_port)
-    logger.info("Initialisation completed, now running . . .")
-    v.run(is_running)
 
 if __name__ == "__main__" :
+    from multiprocessing import Queue, Process, Event
+    import sys
+
+    is_running = Event()
+    is_running.set()
+
     def read(input_q):
+        count = 0
         while True:
-            if not input_q.empty():
-                item = input_q.get_nowait()
-                # print(type(item))
-                # print(type(item))
+            try:
             
-    output_q = Queue()
-    vision = Process(target=vision_worker,args=(output_q,))
+                if not input_q.empty():
+                    item = input_q.get_nowait()
+                    t = str(type(item))
+                    # print(t)
+                    if t == "<class 'TeamControl.SSL.vision.field.GeometryData'>":
+                        count += 1
+                        print(item)
+                        if count == 4 :
+                            is_running.clear()
+                            break
+                    # print(type(item))
+                else:
+                    time.sleep(1)
+                
+            except KeyboardInterrupt:
+                print(f" Force Quitting")
+                sys.exit()
+    
+    logger = None
+    output_q, use_grSim, vision_port = Queue(), True, 10006
+
+    vision = Process(target=VisionProcess.run_worker,args=(is_running,logger,output_q, use_grSim, vision_port))
     reader = Process(target=read,args=(output_q,))
     
     vision.start()
