64d5bdbf5e6cb08d186744b2753b68e6a8247981
Emma <20544166@student.westernsydney.edu.au>
2025-12-29 16:51:42 +1100

    YAML Sender has converted into yaml_config and moved to utils

---
+++ src/TeamControl/utils/yaml_config.py
@@ -0,0 +1,34 @@
+
+from pathlib import Path
+
+import ast
+import yaml
+
+try:
+    from yaml import CLoader as Loader
+except ImportError as e:
+    from yaml import Loader
+
+
+          
+class Config():
+    def __init__(self,config_filename:str="ipconfig.yaml"):
+
+        path = Path(__file__).resolve()
+        # use with the auto open and close
+        with open(path.parent / config_filename, "r") as file:
+            # read and sets the robot addr and base ID
+            self.set_config(yaml.load(file, Loader))
+            
+    def set_config(self,raw):
+        self.blue = raw["blue"]
+        self.yellow = raw["yellow"]
+        self.grSim_addr = (raw["grSim"]["ip"], raw["grSim"]["port"])
+        self.vision = raw["vision"]["multicast-group"], raw["vision"]["port"] 
+        self.game_controller = raw["gc"]["multicast-group"], raw["gc"]["port"]
+                
+    
+
+if __name__ == "__main__":
+    server_config = Config()
+    print(server_config.blue)
